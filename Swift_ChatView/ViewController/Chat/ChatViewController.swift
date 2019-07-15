6aada2f771229ede7a38ffd89d680b4d275a49c2
hidetomo117 <hidetomo117@gmail.com>
2019-07-15 16:20:56 +0900

    create chatViewController layout

--- Swift_ChatView/ViewController/Chat/ChatViewController.swift
+++ Swift_ChatView/ViewController/Chat/ChatViewController.swift
@@ -9,8 +9,22 @@
 import UIKit
 import WebKit
 
+/**
+ チャット画面
+ */
 final class ChatViewController: UIViewController {
 
+    // MARK: - Property
+
+    /// 大元のView
+    @IBOutlet weak var baseView: UIView!
+    /// チャット表示領域
+    @IBOutlet weak var chatView: UIView!
+    /// インプットパーツ表示領域
+    @IBOutlet weak var contentInputView: UIView!
+
+    // MARK: - View life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
